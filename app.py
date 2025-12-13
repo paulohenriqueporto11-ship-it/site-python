62950e86e93dc3df71ed1b2d2f67b2f3bba2c628
paulohenriqueporto11-ship-it <paulohenriqueporto11@gmail.com>
2025-12-13 15:20:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Isso libera o InfinityFree para conversar com o Python
 
-@app.route('/')
-def home():
-    return "Funciona! Minha API Python no Render está online."
+# Essa é a porta de entrada da sua IA
+@app.route('/ia', methods=['POST'])
+def processar_ia():
+    # 1. Pega o que o usuário mandou no site
+    dados = request.json
+    pergunta_do_usuario = dados.get('pergunta', '')
+
+    # --- AQUI VAI ENTRAR O SEU CÓDIGO DA IA DEPOIS ---
+    # Por enquanto, vamos fazer um teste simples:
+    resposta_ia = "O Python recebeu sua mensagem: " + pergunta_do_usuario
+    # -------------------------------------------------
+
+    # 2. Devolve a resposta para o site
+    return jsonify({"resposta": resposta_ia})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
